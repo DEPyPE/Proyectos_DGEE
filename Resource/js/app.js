044a52029ee07365d1bddf1a4a3c5f0d3fb7031c
wamor170795 <wa.morenoramirez@gmail.com>
2021-08-23 13:46:35 -0500

    Mostrar contenido extendido para todas las tarjetas

--- Resource/js/app.js
+++ Resource/js/app.js
@@ -7,16 +7,20 @@ $(function(){
 
 $('.btn-NB').on('click', function(){
 //    $('.infography-title').text('Infografías del Nivel Básico');
-    $('.display-pdf-document').attr('src', 'Resource/pdf/NB.pdf');
+//  <embed class="display-pdf-document" type="application/pdf"/>
+    $('.ShowPDFDocuments-Content').empty();
+    $('.ShowPDFDocuments-Content').html('<embed class="display-pdf-document" type="application/pdf" src="Resource/pdf/NB.pdf"/>');
 });
 
 $('.btn-NMS').on('click', function(){
 //    $('.infography-title').text('Infografías del Nivel Media Superior');
-    $('.display-pdf-document').attr('src', 'Resource/pdf/NMS.pdf');
+$('.ShowPDFDocuments-Content').empty();
+$('.ShowPDFDocuments-Content').html('<embed class="display-pdf-document" type="application/pdf" src="Resource/pdf/NMS.pdf"/>');
 });
 
 $('.btn-NS').on('click', function(){
 //    $('.infography-title').text('Infografías del Nivel Superior');
-    $('.display-pdf-document').attr('src', 'Resource/pdf/NS.pdf');
+$('.ShowPDFDocuments-Content').empty();
+$('.ShowPDFDocuments-Content').html('<embed class="display-pdf-document" type="application/pdf" src="Resource/pdf/NS.pdf"/>');
 });
 
